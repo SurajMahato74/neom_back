066be07bbf699f5dfe0854116c04692c88282d56
SurajMahato74 <surajmahato9823@gmail.com>
2026-01-31 18:32:20 +0545

    udpaetd

--- authentication/notice_serializers.py
+++ authentication/notice_serializers.py
@@ -3,11 +3,51 @@ from .models import Notice
 
 class NoticeSerializer(serializers.ModelSerializer):
     tags_list = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Notice
-        fields = ['id', 'title', 'short_description', 'description', 'notice_type', 'image', 'attachment', 'tags', 'tags_list', 'is_featured', 'is_active', 'published_date', 'created_at', 'updated_at']
+        fields = [
+            'id',
+            'title',
+            'short_description',
+            'description',
+            'notice_type',
+            'image',
+            'attachment',
+            'tags',
+            'tags_list',
+            'is_featured',
+            'is_active',
+            'published_date',
+            'created_at',
+            'updated_at',
+        ]
         read_only_fields = ['id', 'created_at', 'updated_at']
-    
+
     def get_tags_list(self, obj):
         return obj.get_tags_list()
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        request = self.context.get('request')
+
+        if request:
+            if instance.image:
+                data['image'] = (
+                    request.scheme
+                    + "://"
+                    + request.get_host()
+                    + "/api"
+                    + instance.image.url
+                )
+
+            if instance.attachment:
+                data['attachment'] = (
+                    request.scheme
+                    + "://"
+                    + request.get_host()
+                    + "/api"
+                    + instance.attachment.url
+                )
+
+        return data
