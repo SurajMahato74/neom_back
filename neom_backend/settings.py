55cf77409fe6540f64074ab28df61d7b988d54d8
SurajMahato74 <surajmahato9823@gmail.com>
2026-01-31 16:01:11 +0545

    updated

--- neom_backend/settings.py
+++ neom_backend/settings.py
@@ -155,7 +155,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^http://localhost:\d+$",
     r"^http://127\.0\.0\.1:\d+$",
