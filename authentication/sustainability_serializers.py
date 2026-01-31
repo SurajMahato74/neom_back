2a6bfe3668bd27f9e60ca7768f6d33a2ff78cbbf
SurajMahato74 <surajmahato9823@gmail.com>
2026-01-31 18:30:38 +0545

    udapetd

--- authentication/sustainability_serializers.py
+++ authentication/sustainability_serializers.py
@@ -5,13 +5,22 @@ class SustainabilityPillarSerializer(serializers.ModelSerializer):
     class Meta:
         model = SustainabilityPillar
         fields = '__all__'
-    
+
     def to_representation(self, instance):
         data = super().to_representation(instance)
+
         if instance.image:
             request = self.context.get('request')
             if request:
-                data['image'] = request.build_absolute_uri(instance.image.url)
+                # force /api before /media
+                data['image'] = (
+                    request.scheme
+                    + "://"
+                    + request.get_host()
+                    + "/api"
+                    + instance.image.url
+                )
             else:
                 data['image'] = instance.image.url
+
         return data
