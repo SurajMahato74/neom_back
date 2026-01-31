224e9f5d4145a44693340d7cae9619c153674519
SurajMahato74 <surajmahato9823@gmail.com>
2026-01-31 15:41:38 +0545

    updatd the setting files

--- neom_backend/settings.py
+++ neom_backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'authentication.middleware.RequestLoggingMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
